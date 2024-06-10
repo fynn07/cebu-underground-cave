6f2979a785e7e9460e81b773e45fde075bca0d2f
Fynn Borja <pewdiestuff@gmail.com>
2024-06-10 17:17:31 +0800

    Server: Added Post functionality and Routes

--- server/controller/contentController.js
+++ server/controller/contentController.js
@@ -1,8 +1,23 @@
 const pool = require('../config/database');
 const jwt = require('jsonwebtoken');
 
+//req will now contain req.user which contains the ID for for the logged in user so you can query for users
 const createPost = async(req, res) => {
-    return res.status(200).json(req.user);
+    const { Title, Content, ImageLink, Genre} = req.body;
+
+    const AuthorId = req.user.Id;
+
+    try {
+        const result = await pool.query(
+            `INSERT INTO "Post" ("Title", "Content", "ImageLink", "Genre", "AuthorID") VALUES ($1, $2, $3, $4, $5)`,
+            [Title, Content, ImageLink, Genre, AuthorId]
+        );
+
+        return res.json({message : "Post successful"});
+
+    } catch (err) {
+        return res.status(500).json({error : "Internal Server Error"}, err);
+    }
 }
 
 module.exports = { createPost };
