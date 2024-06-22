f4ad60fdeac0f6466206031fc942274e53db5377
Fynn Borja <pewdiestuff@gmail.com>
2024-06-22 20:50:56 +0800

    Client: Fields are saved for Login and Signup when Error in Validation Occurs

--- client/src/components/ui/loginModal.jsx
+++ client/src/components/ui/loginModal.jsx
@@ -7,7 +7,7 @@ const LoginModal = (props) => {
     const [loading, setLoading] = useState(false);
 
     const content = (
-        <div className="relative z-10">
+        <div className={`relative z-10 ${loading ? 'hidden' : 'block'}`}>
             <div onClick={() => props.setLoginModalShow(false)} className="hover:cursor-pointer absolute top-0 right-2 bg-gray-300 px-2 rounded-full z-20">
                 <p className="">X</p>
             </div>
@@ -18,13 +18,14 @@ const LoginModal = (props) => {
 
     return props.loginModalShow ? (
         <div className="flex flex-col absolute rounded-xl top-0 left-0 bottom-0 right-0 m-auto w-72 p-8 h-3/4 bg-button_color lg:w-80 xl:w-96 2xl:w-96">
-            {loading ? (<LoadingScreen/>) : (content)}
+            {loading && <LoadingScreen />}
+            {content}
         </div>
     ) : (
         <div className="flex-col absolute rounded-xl top-0 left-0 bottom-0 hidden right-0 m-auto w-96 p-8 h-3/4 bg-button_color">
             {content}
         </div>
-    )
+    );
 }
 
 export default LoginModal;
--- client/src/components/ui/signupModal.jsx
+++ client/src/components/ui/signupModal.jsx
@@ -7,20 +7,21 @@ const SignupModal = (props) => {
     const [loading, setLoading] = useState(false);
 
     const content = (
-        <div className="relative flex flex-col h-full">
+        <div className={`relative flex flex-col h-full ${loading ? 'hidden' : 'block'}`}>
             <div onClick={() => props.setSignupModalShow(false)} className="hover:cursor-pointer absolute top-0 right-2 bg-gray-300 px-2 rounded-full">
                 <p className="">X</p>
             </div>
-            <FlavorText header={"SIGN UP"} subtext={"Join Underground Cebuano Enjoyers today and chill with like-minded people!"}/>
+            <FlavorText header={"SIGN UP"} subtext={"Join Underground Cebuano Enjoyers today and chill with like-minded people!"} />
             <div className="flex-1 overflow-y-auto">
-                <SignupForm setSignupModalShow={props.setSignupModalShow} setLoginModalShow={props.setLoginModalShow} setLoading={setLoading}/>
+                <SignupForm setSignupModalShow={props.setSignupModalShow} setLoginModalShow={props.setLoginModalShow} setLoading={setLoading} />
             </div>
         </div>
     );
-    
+
     return props.signupModalShow ? (
         <div className="flex flex-col absolute rounded-xl top-0 left-0 bottom-0 right-0 m-auto w-72 p-8 h-3/4 bg-button_color lg:w-80 xl:w-96 2xl:w-96">
-            {loading ? (<LoadingScreen/>) : (content)}
+            {loading && <LoadingScreen />}
+            {content}
         </div>
     ) : (
         <div className="flex-col absolute rounded-xl top-0 left-0 bottom-0 hidden right-0 m-auto w-96 p-8 h-3/4 bg-button_color">
